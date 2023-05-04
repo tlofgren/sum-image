cb64ef360aaaadd00a9480555191cdd9138c9d51
Tyler Lofgren <5882380+tlofgren@users.noreply.github.com>
2023-05-04 15:49:54 -0500

    Add bolt app configuration

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 import logging
+import os
 
 logging.basicConfig(level=logging.DEBUG)
 
 from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-app = App()
+
+app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
 
 @app.middleware  # or app.use(log_request)
 def log_request(logger, body, next):
     logger.debug(body)
     return next()
+
+
+@app.command("/sum-image")
+def slash_command(ack, body):
+    user_id = body["user_id"]
+    ack(f"Hi, <@{user_id}>!")
 
 
 @app.event("app_mention")
@@ -30,11 +39,18 @@ from flask import Flask, request
 flask_app = Flask(__name__)
 handler = SlackRequestHandler(app)
 
+@flask_app.route("/")
+def home():
+    return "Hello, Flask!"
+
 
 @flask_app.route("/slack/events", methods=["POST"])
 def slack_events():
     return handler.handle(request)
 
+
+if __name__ == "__main__":
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
 
 # pip install -r requirements.txt
 # export SLACK_SIGNING_SECRET=***
