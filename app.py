15f8e150fb570888e467fcd65a28779f7cb76c7a
Tyler Lofgren <5882380+tlofgren@users.noreply.github.com>
2023-05-04 19:03:05 -0500

    Subscribe to additional events
    Only respond to DMs, not just any message in a channel

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import logging
 import os
+from typing import Callable
 
 logging.basicConfig(level=logging.DEBUG)
 
-from slack_bolt import App
+from slack_bolt import App, Say, BoltContext
 from slack_bolt.adapter.flask import SlackRequestHandler
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_sdk import WebClient
+
 
 from api import respond_direct_invocation
 
@@ -18,6 +21,12 @@ def log_request(logger, body, next):
     return next()
 
 
+def extract_subtype(body: dict, context: BoltContext, next: Callable):
+    """https://github.com/slackapi/bolt-python/blob/main/examples/message_events.py"""
+    context["subtype"] = body.get("event", {}).get("subtype", None)
+    next()
+
+
 @app.command("/sum-image")
 def handle_slash_command(ack, say, client, body, logger):
     logger.debug("slash command body=%s", body)
@@ -28,14 +37,32 @@ def handle_slash_command(ack, say, client, body, logger):
 
 @app.event("app_mention")
 def handle_app_mention(body, say, logger):
-    logger.info(body)
+    logger.debug(body)
     say("What's up?")
 
 
 @app.event("message")
-def handle_message(message, say):
-    response = respond_direct_invocation(message)
-    say(response)
+def handle_message(message, say, logger):
+    logger.debug("event message message=%s", message)
+    if _is_message_im(message):
+        logger.debug("YES, this is an IM")
+        response = respond_direct_invocation(message)
+        say(response)
+    else:
+        logger.debug("NOT IM")
+
+
+def _is_message_im(message):
+    return message["channel_type"] == "im"
+
+
+# This listener handles all uncaught message events
+# (The position in source code matters)
+@app.event({"type": "message"}, middleware=[extract_subtype])
+def just_ack(logger, context):
+    """https://github.com/slackapi/bolt-python/blob/main/examples/message_events.py"""
+    subtype = context["subtype"]  # by extract_subtype
+    logger.info(f"{subtype} is ignored")
 
 
 from flask import Flask, request
